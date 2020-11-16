41e4b411d410899abeb48081b9a834f9d67453be
Takoy <kyberfx@gmail.com>
2020-11-16 21:54:07 +0900

    init commit

---
+++ app.js
@@ -0,0 +1,11 @@
+alert("Подтверждай действие сук");
+
+var a = "Honda Civic Ferio";
+console.log(a);
+
+console.log({
+  model: "Ferio",
+  engine: "D15b",
+  horsepower: "130",
+  mode: "VTEC",
+});
